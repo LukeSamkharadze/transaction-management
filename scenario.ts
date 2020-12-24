2ccd6d2a0dbbc2912fbed9e606c7887e96d9eacb
Luka Samkharadze <samkharadzeluke@gmail.com>
2020-12-24 21:25:31 +0400

    add decorators

--- scenario.ts
+++ scenario.ts
@@ -1,19 +1,35 @@
-export class Meta {
-  constructor(public title: string, public description: string) { }
-}
+export namespace Scenario {
 
-export class Scenario {
-  meta: Meta;
-  isCritical?: boolean;
+  export type CallFn = (store: any) => Promise<any>;
+  export type RestoreFn = (store: any) => Promise<any>;
 
-  constructor(
-    public index: number,
-    title: string,
-    description: string,
-    public call: (store: any) => void,
-    public restore?: (store: any) => void,
-    isCritical?: boolean) {
-    this.meta = new Meta(title, description);
-    this.isCritical = isCritical || true;
+  export class Meta {
+    constructor(public title: string, public description: string) { }
+  }
+
+  function secured(writable: boolean, configurable: boolean, enumerable: boolean) {
+    return function (target: any, propertyKey: string): any {
+      return {
+        writable,
+        configurable,
+        enumerable
+      }
+    }
+  }
+
+  export class Scenario {
+    @secured(true, false, false) index: number;
+    @secured(true, false, false) meta: Meta;
+    @secured(true, false, false) call: CallFn;
+    @secured(true, false, false) restore?: RestoreFn;
+    @secured(true, false, false) isCritical?: boolean;
+
+    constructor(index: number, title: string, description: string, call: CallFn, restore?: RestoreFn, isCritical: boolean = true) {
+      this.index = index;
+      this.meta = new Meta(title, description);
+      this.call = call;
+      this.restore = restore;
+      this.isCritical = isCritical;
+    }
   }
 }
